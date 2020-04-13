ccecb5f2647218d5cf33a84c4eabe2aa0deabc11
updog123 <obol03al@gmail.com>
2020-04-13 18:35:05 +0300

    added all functionality

--- bot.py
+++ bot.py
@@ -10,3 +10,28 @@ reddit = praw.Reddit(client_id = os.getenv('PNB_CLIENT_ID'),
                      user_agent = os.getenv('PNB_USER_AGENT'))
 
 prime_number_daily = reddit.subreddit('primenumberdaily')
+
+def next_prime(prime):
+    n = prime + 1
+    while not is_prime(n):
+        n += 1
+    return n
+
+def is_prime(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+def main():
+    with open('last_prime.txt', 'r+') as f:
+        prime = f.read()
+        prime_number_daily.submit(prime, '')
+        f.seek(0)
+        f.truncate()
+        f.write(str(next_prime(int(prime))))
+
+if __name__ == '__main__':
+    main()

06ae38fec1f645d1199757e86ff069c97b04199a
updog123 <obol03al@gmail.com>
2020-04-13 20:31:43 +0300

    changed random

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import praw
 from dotenv import load_dotenv
 import os
+import random
+
+range_size = 1000000
 
 load_dotenv()
 reddit = praw.Reddit(client_id = os.getenv('PNB_CLIENT_ID'),
@@ -11,12 +14,6 @@ reddit = praw.Reddit(client_id = os.getenv('PNB_CLIENT_ID'),
 
 prime_number_daily = reddit.subreddit('primenumberdaily')
 
-def next_prime(prime):
-    n = prime + 1
-    while not is_prime(n):
-        n += 1
-    return n
-
 def is_prime(n):
     i = 2
     while i * i <= n:
@@ -26,12 +23,8 @@ def is_prime(n):
     return True
 
 def main():
-    with open('last_prime.txt', 'r+') as f:
-        prime = f.read()
-        prime_number_daily.submit(prime, '')
-        f.seek(0)
-        f.truncate()
-        f.write(str(next_prime(int(prime))))
+    primes = [i for i in range(2,range_size) if is_prime(i)]
+    prime_number_daily.submit(str(random.choice(primes)), '')
 
 if __name__ == '__main__':
     main()
--- restart.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open('last_prime.txt', 'r+') as f:
-        f.seek(0)
-        f.truncate()
-        f.write('2')
-
-if __name__ == '__main__':
-    main()
